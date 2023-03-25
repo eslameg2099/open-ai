33b2455f7f5b5f1733864577474b68d11ced8519
unknown <eslameg2099@yahoo.com>
2023-03-25 02:33:24 +0200

    first commit

---
+++ app/Http/Controllers/OpenAIController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use OpenAI\Laravel\Facades\OpenAI;
+
+class OpenAIController extends Controller
+{
+    public function index(Request $Request)
+    {
+     
+        $result = OpenAI::completions(3)->create([
+            'model' => 'text-davinci-003',
+            'prompt' => $Request->text,
+            'max_tokens'=>2048,
+            'n'=>2,
+        ]);
+         
+        return response()->json([
+            'result' => $result,
+        ]);
+
+    }
+}
