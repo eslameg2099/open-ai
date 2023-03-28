2489c2f5c8fb5b70f0ad1a50bf0f2ec23bb6fec4
eslameg2099 <57689846+eslameg2099@users.noreply.github.com>
2023-03-28 20:26:36 +0200

    Update OpenAIController.php

--- app/Http/Controllers/OpenAIController.php
+++ app/Http/Controllers/OpenAIController.php
@@ -10,15 +10,17 @@ class OpenAIController extends Controller
     public function index(Request $Request)
     {
      
-        $result = OpenAI::completions(6)->create([
+         $result = OpenAI::completions(6)->create([
             'model' => 'text-davinci-003',
+            'temperature'=>0.7,
+            'top_p'=>1,
             'prompt' => $Request->text,
             'max_tokens'=>2048,
-            'n'=>2,
+            'n'=>1,
         ]);
          
         return response()->json([
-            'result' => $result['choices'],
+            'result' => trim($result->choices[0]->text) ,
         ]);
 
     }
