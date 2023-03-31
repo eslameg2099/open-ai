a26dd29b93668f1720217b0488936f2529f696e8
eslameg2099 <57689846+eslameg2099@users.noreply.github.com>
2023-03-31 20:50:09 +0200

    Update OpenAIController.php

--- app/Http/Controllers/OpenAIController.php
+++ app/Http/Controllers/OpenAIController.php
@@ -11,11 +11,13 @@ class OpenAIController extends Controller
     {
      
          $result = OpenAI::completions()->create([
-            'model' => 'text-davinci-003',
-            'temperature'=>1,
-            'top_p'=>1,
-            'prompt' => $Request->text,
-            'max_tokens'=>2048,
+            "model" => "text-davinci-003",
+            "temperature" => 0.7,
+            "top_p" => 1,
+            "frequency_penalty" => 0,
+            "presence_penalty" => 0,
+            'max_tokens' => 2048,
+            'prompt' => sprintf('Write article about: %s', $Request->text),
             'n'=>1,
         ]);
          
